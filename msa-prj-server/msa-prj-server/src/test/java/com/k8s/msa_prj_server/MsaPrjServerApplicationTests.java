ccb682df99958341fe80a286ecf92c1cc2b6d16e
jinaen <fire_long346@naver.com>
2025-07-26 18:30:42 +0900

    w3 jinaen hw

---
+++ msa-prj-server/msa-prj-server/src/test/java/com/k8s/msa_prj_server/MsaPrjServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k8s.msa_prj_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaPrjServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

decc99725487622318e29ad77dca75a23d53d308
jinaen <fire_long346@naver.com>
2025-07-26 18:26:23 +0900

    week3 진영 실습

---
+++ msa-prj-client/src/test/java/com/k8s/msa_prj_client/MsaPrjClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k8s.msa_prj_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaPrjClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

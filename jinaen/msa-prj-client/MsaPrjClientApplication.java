7cd2fd385419568f04aaf72a04224205e397158b
jinaen <fire_long346@naver.com>
2025-08-05 21:46:04 +0900

    gateway-test

--- jinaen/msa-prj-client/MsaPrjClientApplication.java
+++ jinaen/msa-prj-client/MsaPrjClientApplication.java
@@ -2,9 +2,8 @@ package com.k8s.msa_prj_client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//
-//@EnableDiscoveryClient
+
+
 @SpringBootApplication
 public class MsaPrjClientApplication {
 	public static void main(String[] args) {
---
+++ jinaen/msa-prj-gatewaytest/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.k8s.msa_prj_gatewaytest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/test")
+    public String test(){
+        return "Hello World";
+    }
+}
